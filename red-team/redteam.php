ec892d858ac35585b38c65ca625b8c505dd1874e
Bogdan Bărbulescu <130338564+bogdanbarbulescu@users.noreply.github.com>
2024-03-13 20:53:31 +0200

    Add files via upload

---
+++ red-team/redteam.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Portofolio</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'>
+  <link rel="stylesheet" href="style.css">
+
+  <script type="module" src="main.js"></script>
+
+</head>
+
+<body>
+<?php
+  include './include/navbar.php';
+  ?>
+  <main id="main" class="main">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card h-100">
+            <img src="../img/fitness-tag.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Reporting and Documentation</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+              <a href="redteam.php" class="stretched-link"></a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../img/opensource.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Open Source Tools</h5>
+              <p class="card-text">This is a short card.</p>
+              <a href="redteam.php" class="stretched-link"></a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../img/health-tag.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Resources and Tools</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                content.</p>
+              <a href="redteam.php" class="stretched-link"></a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../img/nature-tag.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                content.</p>
+              <a href="redteam.php" class="stretched-link"></a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../img/nature-tag.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                content.</p>
+              <a href="redteam.php" class="stretched-link"></a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../img/fitness-tag.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                content.</p>
+              <a href="redteam.php" class="stretched-link"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+</body>
+
+</html>
