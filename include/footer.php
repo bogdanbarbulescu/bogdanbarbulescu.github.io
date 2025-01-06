6998e48ec78beda6983d5bcfb82932e69cce117b
Bogdan Bărbulescu <130338564+bogdanbarbulescu@users.noreply.github.com>
2025-01-06 20:41:17 +0200

    Delete include directory

--- include/footer.php
+++
@@ -1,9 +0,0 @@
-  <!-- Footer -->
-  <!-- Copyright -->
-  <footer>
-    <div class="text-center p-4 text-muted" style="background-color: rgba(0, 0, 0, 0.05);">
-      © 2024 Copyright:
-      <a class="text-reset fw-bold">Bogdan Barbulescu</a>
-    </div>
-    <!-- Copyright -->
-  </footer>
